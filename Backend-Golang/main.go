a6f27eee2880ac78634b4033cd3cc16ff0b23195
RajMohan <rmjayakumar@tekion.com>
2023-09-05 12:52:25 +0530

    Added ping pong as separate function

--- Backend-Golang/main.go
+++ Backend-Golang/main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Pong")
-	})
 
-	http.ListenAndServe("localhost:4040", nil)
+	// defining routes
+	http.HandleFunc("/ping", pingPong)
+
+	// starting server
+	log.Fatal(http.ListenAndServe("localhost:4040", nil))
+}
+
+func pingPong(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Pong")
 }
